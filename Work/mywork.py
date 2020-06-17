62e7b716b65e2882be84a2bcd220213b5a44d636
alysoncp <alyson.c.pickett@gmail.com>
2020-06-17 11:35:26 -0700

    Create mywork.py

---
+++ Work/mywork.py
@@ -0,0 +1,17 @@
+# mywork
+
+total_cost = 0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        nshares = int(row[1])
+        price = float(row[2])
+        total_cost = nshares*price
+        row.append(total_cost)
+        print(row, end='')
+            
+          
+            
+
